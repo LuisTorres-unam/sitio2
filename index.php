eaac505717b8183fd47bc5fb504e0e83c6caa814
LuisTorres-unam <luisbta1998@comunidad.unam.mx>
2021-03-03 18:22:50 -0600

    ADD: index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+	echo "Hola Me llamo Luis Bernardo Torres Arellano, \n tengo 22 años \n";
+	echo "Me quiero especializar en desarrollo de sistemas\n";
+	echo "Me gusta la música electrónica";
+	echo "y los videojuegos";
+
+?>
